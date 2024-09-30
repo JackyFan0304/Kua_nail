2849db00decf528ad858e5016c9ccdf94d95d86a
JackyFan0304 <jacky80124@gmail.com>
2024-09-30 17:37:47 +0800

    feat(hero & about): add hero and about sections with responsive design
    - Implemented the hero section featuring a stunning introduction and call-to-action.
    - Developed the about section highlighting my passion as a nail artist with responsive text adjustments.
    - Ensured both sections are fully responsive for optimal viewing on all devices.
    
    This commit enhances the user interface and experience of the application,
    laying the groundwork for future features and components.
    
    Closes #2

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
-const { createApp } = Vue;
-createApp({
-    data() {
-        return {
-            message: 'hello Vue!',
-            test_message: 'test'
-        }
-    }
-}).mount('#app'); // 將應用掛載到 #app 元素上
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'normalize.css';
+
+createApp(App).mount('#app');
